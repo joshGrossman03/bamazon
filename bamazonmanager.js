c4f2e6aa94736f44dd0749a817504129287ffd26
Josh Grossman <grossmanjoshuaj@gmail.com>
2019-08-13 13:08:29 -0500

    view low inventory is functioning

--- bamazonmanager.js
+++ bamazonmanager.js
@@ -13,13 +13,45 @@ connection.connect(function(err){
     if (err) throw err;
     //console.log("connected as id " + connection.threadId);
     //connection.end();
-    productInventory();
+    managerConsole();
 });
+
+
+var managerConsole = function(){
+    console.log("Welcome to the Manager Console, What would you like to do today?\n");
+    inquirer.prompt([
+        {
+            type:"list",
+            name:"managerMenu",
+            message:"\nPlease make a selction from the actions listed below:\n",
+            choices:["View Products for Sale","View Low Inventory","Add to Inventory","Add New Product"]
+        }
+    ]).then(function(answers){
+
+        switch (answers.managerMenu){
+            case "View Products for Sale": productInventory();
+            break;
+            case "View Low Inventory" : lowInventory();
+            break;
+            /*case "Add to Inventory": updateInventory();
+            break;
+            case "Add New Product": addProduct();
+            break;*/
+        };
+    });
+};
 
 var productInventory = function(){
     connection.query('SELECT * FROM `products`', function(err, results){
         if(err) throw err;
         console.table(results);
     });  
+};
+
+var lowInventory = function(){
+    connection.query('SELECT * FROM `products` WHERE `stock_qty` <= 5',function(err,results){
+        if (err) throw err;
+        console.table(results);
+    });
 };
 
